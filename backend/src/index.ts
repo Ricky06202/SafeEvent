384c9ad41ce9e2d3e1fcbb75fe3fc112b29af696
Ricardo A. Sanjur G. <ricardosanjurg@gmail.com>
2026-03-08 15:42:34 -0500

    ya encontramos por q no funcionaba

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,10 @@ app.get('/api', (c) => {
   return c.text('Hello from Hono (/api)!')
 })
 
+app.get('/api/', (c) => {
+  return c.text('Hello from Hono (/api/) con barra al final!')
+})
+
 app.all('*', (c) => {
   return c.text(`404 Custom - Debug Info: Method=${c.req.method}, Path=${c.req.path}, URL=${c.req.url}`)
 })
